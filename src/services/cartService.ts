523cc5079043fa7b5d555937c8474934445ca052
Mohammed Sadiq <moosadiq11@gmail.com>
2025-01-13 02:55:27 +0300

    update item in cart

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -70,3 +70,51 @@ export const addItemToCart = async ({
   const updateCart = await cart.save();
   return { data: updateCart, statusCode: 200 };
 };
+
+interface UpdateItemInCart {
+  productId: any;
+  quantity: number;
+  userId: string;
+}
+
+export const updateItemInCart = async ({
+  productId,
+  quantity,
+  userId,
+}: UpdateItemInCart) => {
+  const cart = await getActiveCartForUser({ userId });
+  const existsInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+
+  if (!existsInCart) {
+    return { data: "Item does not exist in cart", statusCode: 400 };
+  }
+
+  // fetch the product
+  const product = await productModel.findById(productId);
+
+  if (!product) {
+    return { data: "Product not found", statusCode: 400 };
+  }
+
+  if (product.stock < quantity) {
+    return { data: "Low stock for item", statusCode: 400 };
+  }
+
+  const otherCartItems = cart.items.filter(
+    (p) => p.product.toString() !== productId
+  );
+
+  let total = otherCartItems.reduce((sum, product) => {
+    sum += product.quantity * product.unitPrice;
+    return sum;
+  }, 0);
+
+  existsInCart.quantity = quantity;
+  total += existsInCart.quantity * existsInCart.unitPrice;
+
+  cart.totalAmount = total;
+  const updateCart = await cart.save();
+  return { data: updateCart, statusCode: 200 };
+};
