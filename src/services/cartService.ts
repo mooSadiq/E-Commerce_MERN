89b9adf08db3554c682f48021fe1af7e3b794a81
Mohammed Sadiq <moosadiq11@gmail.com>
2025-01-13 02:27:56 +0300

    add item to cart

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { cartModel } from "../models/cartModel";
+import productModel from "../models/productModel";
 
 interface CreatCartForUser {
   userId: string;
@@ -23,4 +24,49 @@ export const getActiveCartForUser = async ({
   }
 
   return cart;
+};
+
+interface AddItemToCart {
+  productId: any;
+  quantity: number;
+  userId: string;
+}
+
+export const addItemToCart = async ({
+  productId,
+  quantity,
+  userId,
+}: AddItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });
+
+  const existsInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+
+  if (existsInCart) {
+    return { data: "Item already exist in cart", statusCode: 400 };
+  }
+
+  // fetch the product
+  const product = await productModel.findById(productId);
+
+  if (!product) {
+    return { data: "Product not found", statusCode: 400 };
+  }
+
+  if (product.stock < quantity) {
+    return { data: "Low stock for item", statusCode: 400 };
+  }
+
+  cart.items.push({
+    product: productId,
+    unitPrice: product.price,
+    quantity,
+  });
+
+  // update the totalAmount for the cart
+  cart.totalAmount += product.price * quantity;
+
+  const updateCart = await cart.save();
+  return { data: updateCart, statusCode: 200 };
 };
